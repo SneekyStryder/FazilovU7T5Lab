ada8926cf11bb7c75df9bc8ad82a67e81a1289a4
Imran Fazilov <ifazilov3584@bths.edu>
2023-02-09 12:15:03 -0500

    done

--- src/CarRepair.java
+++ src/CarRepair.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class CarRepair {
+public class CarRepair
+{
+    private int mechanicNum;
+    private int bayNum;
+
+    public CarRepair(int m, int b)
+    {
+        mechanicNum = m;
+        bayNum = b;
+    }
+
+    public int getMechanicNum()
+    {
+        return mechanicNum;
+    }
+
+    public int getBayNum()
+    {
+        return bayNum;
+    }
+
+    public String toString()
+    {
+        return "mechanic num: " + mechanicNum + ", bay num: " + bayNum;
+    }
 }
